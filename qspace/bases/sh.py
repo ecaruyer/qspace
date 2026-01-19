246aeefd1df4e248cf45ad69e67620632f272936
Emmanuel Caruyer <caruyer@gmail.com>
2026-01-19 16:17:11 +0100

    NF: Added support for non symmetric spherical harmonics

--- qspace/bases/sh.py
+++ qspace/bases/sh.py
@@ -23,27 +23,33 @@ class SphericalHarmonics:
         A 1d array of coefficients representing the function.
     """
     
-    def __init__(self, coefficients):
+    def __init__(self, coefficients, symmetric=True):
+        self.symmetric = symmetric
         self._create_from_coefficients(coefficients)
 
     
     def _create_from_coefficients(self, coefficients):
         rank = 0
         while True:
-            dim_sh = dimension(rank)
+            dim_sh = dimension(rank, self.symmetric)
             if len(coefficients) == dim_sh:
                 self.rank = rank
                 self.coefficients = coefficients
                 return
             elif len(coefficients) < dim_sh:
                 raise ValueError("Invalid dimension for SH coefficients.")
-            rank += 2
+            if self.symmetric:
+                rank += 2
+            else:
+                rank += 1
 
 
     def get_rank(self):
         return self._rank
 
+
     def set_rank(self, value):
+        if self.
         if value % 2 != 0:
             raise ValueError("'rank' only accepts even values.")
         self._rank = value
@@ -53,6 +59,7 @@ class SphericalHarmonics:
 
     def get_coefficients(self):
         return self._coefficients
+
 
     def set_coefficients(self, value):
         if value.shape[0] != dimension(self.rank):
@@ -75,9 +82,13 @@ class SphericalHarmonics:
         coefs = self.coefficients
         result = 0
         rank = self.rank
-        for l in range(0, rank+1, 2):
+        if self.symmetric:
+            step = 2
+        else:
+            step = 1
+        for l in range(0, rank+1, step):
             for m in range(-l, l+1):
-                j = index_j(l, m)
+                j = index_j(l, m, self.symmetric)
                 if coefs[j] != 0.0:
                     if m < 0:
                         result += coefs[j] * np.sqrt(2)         \
@@ -98,36 +109,43 @@ class SphericalHarmonics:
         return result
 
 
-def dimension(rank):
+def dimension(rank, symmetric=True):
     """Returns the dimension of the spherical harmonics basis for a given 
     rank.
     """
-    return int((rank + 1) * (rank + 2) / 2)
+    if symmetric:
+        return int((rank + 1) * (rank + 2) / 2)
+    return (rank + 1) * (rank + 1)
 
 
-def index_j(l, m):
+def index_j(l, m, symmetric=True):
     "Returns the flattened index j of spherical harmonics."
     # l is between 0 and rankSH, m is btw -l and l
     if np.abs(m) > l:
         raise NameError('SphericalHarmonics.j: m must lie in [-l, l]')
-    return int(l + m + (2 * np.array(range(0, l, 2)) + 1).sum())
+    if symmetric:
+        return int(l + m + (2 * np.array(range(0, l, 2)) + 1).sum())
+    return l * (l + 1) + m
 
 
-def index_l(j):
+def index_l(j, symmetric=True):
     "Returns the degree l of SH associated to index j"
     l = 0
-    while dimension(l) - 1 < j:
-        l += 2
+    while dimension(l, symmetric) - 1 < j:
+        if symmetric:
+            l += 2
+        else:
+            l += 1
     return l
 
 
-def index_m(j):
+def index_m(j, symmetric=True):
     "Returns the order m of SH associated to index j"
-    l = index_l(j)
-    return j - dimension(l) + l + 1
+    l = index_l(j, symmetric)
+    return j - dimension(l, symmetric) + l + 1
 
 
-def matrix(theta, phi, rank=_default_rank):
+def matrix(theta, phi, rank=_default_rank, symmetric=True):
     """Returns the spherical harmonics observation matrix for a given set
     of directions represented by their polar and azimuthal angles.
 
@@ -146,8 +164,8 @@ def matrix(theta, phi, rank=_default_rank):
         The observation matrix corresponding to the direction set passed as
         input.
     """
-    dim_sh = dimension(rank)
-    sh = SphericalHarmonics(np.zeros(dim_sh))
+    dim_sh = dimension(rank, symmetric)
+    sh = SphericalHarmonics(np.zeros(dim_sh), symmetric)
     N = theta.shape[0]
     H = np.zeros((N, dim_sh))
     for j in range(dim_sh):
